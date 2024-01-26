84d2487db14fd5edda5a3ec1e924bf0ef0f520fb
Pablo <pablocg7518@gmail.com>
2024-01-26 14:17:52 +0100

    Commit 1

---
+++ Bateria.java
@@ -0,0 +1,9 @@
+public class Bateria{
+    private int duracion;
+    private int capacidad;
+    
+    public Bateria(int duracion, int capacidad){
+        this.duracion = duracion;
+        this.capacidad = capacidad;
+    }
+}
---
+++ TelefonoMovil.java
@@ -0,0 +1,10 @@
+public class TelefonoMovil {
+    private String marca;
+    private int almacenamiento;
+    private Bateria bateria;
+    
+    public TelefonoMovil(String marca, int almacenamiento){
+       this.marca = marca;
+       this.almacenamiento = almacenamiento;
+    }
+}
